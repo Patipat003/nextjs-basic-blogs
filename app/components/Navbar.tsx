e7bf22d925860edb44a2b4d9cab749a78c01b75e
Patipat003 <28613patipat@gmail.com>
2025-07-20 22:34:25 +0700

    Update Navbar.tsx

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <div className="fixed min-w-screen bg-black/10 text-white top-0 z-50 border-b border-white/10 shadow-2xl backdrop-blur-sm">
       <Link href="/">
         <div className="max-w-7xl mx-auto px-6 py-4 text-white">
-          <h1 className="text-3xl font-bold">News Hub</h1>
+          <h1 className="text-indigo-400 text-3xl font-bold">News Hub</h1>
           <p className="mt-2 text-md">Stay updated with the latest news</p>
         </div>
       </Link>
