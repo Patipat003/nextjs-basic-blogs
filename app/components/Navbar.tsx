7c3e244c53515f0fac88659be9456d3dea4c251b
Patipat003 <28613patipat@gmail.com>
2025-08-04 22:12:18 +0700

    Update Navbar.tsx

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <div className="sticky min-w-screen bg-black/10 text-white top-0 z-50 border-b border-white/10 shadow-2xl backdrop-blur-sm md:h-26">
       <div className="max-w-7xl mx-auto px-6 py-4 text-white">
-        <Link href="/" passHref legacyBehavior>
+        <Link href="/">
           <a className="block">
             <h1 className="text-indigo-400 text-2xl md:text-3xl font-bold">
               News Hub
